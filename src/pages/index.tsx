659c4a0cd06b236ea12418f8b072335b5c722496
graceDev1 <grace.devolop@gmail.com>
2021-08-21 13:21:59 +0200

    done with footer and the desktop view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/banner/Banner'
 import Client from '../components/client/Client'
+import Footer from '../components/Footer'
 import Product from '../components/product/Product'
 import Section from '../components/sections/Section'
 
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
      <Section/>
      <Client/>
      <Product/>
+     <Footer/>
     </div>
   )
 }
