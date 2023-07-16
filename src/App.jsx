f794e64bc73c6fa3b2fe438264d9aae45a39d118
gracebir <grace.devolop@gmail.com>
2023-07-17 00:07:40 +0200

    project structures

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Header from './components/Header'
+import Activity from './components/Activities'
+import Hero from './components/Hero'
+import Testimonials from './components/Testimonials'
+import Prodcut from './components/Product'
+import Footer from './components/Footer'
+
 function App() {
 
   return (
     <>
-     
+     <Header/>
+     <Hero/>
+     <Activity/>
+     <Testimonials/>
+     <Prodcut/>
+     <Footer/>
     </>
   )
 }
