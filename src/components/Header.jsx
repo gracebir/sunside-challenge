7d749a4159389ccb838becf1589bc8087434665a
gracebir <grace.devolop@gmail.com>
2023-09-10 14:51:30 +0200

    header desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import logo from '../assets/logo.svg'
+import hamburger  from '../assets/icon-hamburger.svg'
 
 function Header() {
   return (
-    <header>
-      Header
+    <header className="bg-moderate-cyan py-8 max-w-full flex items-center justify-between px-8 text-white-color">
+      <img src={logo} alt='logo' />
+      <nav className='hidden lg:flex flex-row items-center space-x-10'>
+        {["About", "Services", "Projects"]
+          .map((item) => (
+            <a key={item} className='font-normal' href="#">{item}</a>
+          ))}
+        <button className='px-6 py-3 bg-white text-dark-blue duration-200 hover:text-white hover:bg-desautred-blue rounded-3xl font-fraunces uppercase'>contact</button>
+      </nav>
+      <button className='block lg:hidden outline-none border-none'>
+        <img src={hamburger} alt='hamburger'/>
+      </button>
     </header>
   )
 }
