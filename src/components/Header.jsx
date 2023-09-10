b9fa6db7a1ae1fcb5c7ea18c74ffdcc7a0be93ad
gracebir <grace.devolop@gmail.com>
2023-09-10 17:55:28 +0200

    header mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.svg'
 import hamburger  from '../assets/icon-hamburger.svg'
+import HeroModal from './HeroModal'
 
 function Header() {
+  const [isOpen, seIsOpen] = useState(false)
   return (
-    <header className="bg-moderate-cyan py-8 max-w-full flex items-center justify-between px-8 text-white-color">
+    <header className="py-8 fixed w-full  max-w-full flex items-center justify-between px-6 lg:px-8 text-white-color">
       <img src={logo} alt='logo' />
       <nav className='hidden lg:flex flex-row items-center space-x-10'>
         {["About", "Services", "Projects"]
@@ -13,9 +15,10 @@ function Header() {
           ))}
         <button className='px-6 py-3 bg-white text-dark-blue duration-200 hover:text-white hover:bg-desautred-blue rounded-3xl font-fraunces uppercase'>contact</button>
       </nav>
-      <button className='block lg:hidden outline-none border-none'>
+      <button onClick={() => seIsOpen(!isOpen)} className='block lg:hidden outline-none border-none'>
         <img src={hamburger} alt='hamburger'/>
       </button>
+      {isOpen && <HeroModal/>}
     </header>
   )
 }
