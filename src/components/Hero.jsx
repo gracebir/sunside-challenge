1db375dcf77018bd9e46887b7e3d53e6bd96807f
gracebir <grace.devolop@gmail.com>
2023-09-10 20:38:34 +0200

    hero fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import arrow from '../assets/icon-arrow-down.svg'
 
 const Hero = () => {
   return (
-    <div className='hero-bg py-20 flex items-center lg:items-start lg:py-72 h-screen'>
+    <div className='hero-bg py-20 flex items-center lg:items-start lg:py-64 h-screen'>
       <div className='mx-auto text-white-color flex flex-col space-y-16 lg:space-y-20 items-center'>
-        <h1 className='font-fraunces text-5xl text-center lg:text-7xl uppercase tracking-widest'>we are creatives</h1>
+        <h1 className='font-fraunces text-5xl text-center lg:text-9xl uppercase tracking-widest'>we are creatives</h1>
         <img src={arrow} alt="arrow" />
       </div>
     </div>
