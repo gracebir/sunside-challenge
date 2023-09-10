cd3577755368d1dc58b9b7c29a178c12f99742cc
gracebir <grace.devolop@gmail.com>
2023-09-10 20:34:57 +0200

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import arrow from '../assets/icon-arrow-down.svg'
 
 const Hero = () => {
   return (
-    <div>
-      hero
+    <div className='hero-bg py-20 flex items-center lg:items-start lg:py-72 h-screen'>
+      <div className='mx-auto text-white-color flex flex-col space-y-16 lg:space-y-20 items-center'>
+        <h1 className='font-fraunces text-5xl text-center lg:text-7xl uppercase tracking-widest'>we are creatives</h1>
+        <img src={arrow} alt="arrow" />
+      </div>
     </div>
   )
 }
