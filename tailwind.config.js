8db130420048558d2a2ca4c9a225a6f92ef49b83
gracebir <grace.devolop@gmail.com>
2023-09-10 13:49:26 +0200

    define color style in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ export default {
       fontFamily: {
         'barlow':['Barlow', 'sans-serif'],
         'fraunces': ['Fraunces', 'serif']
+      },
+      colors: {
+        "red-color":"var(--soft-red)",
+        "yellow-color":"var(--yellow)",
+        "cyan-color":"var(--dark-desatured-cyan)",
+        "dark-blue":"var(--dark-blue)",
+        "moderate-cyan":"var(--dark-moderate-cyan)",
+        "desautred-blue":"var(--very-dark-desaturated-blue)",
+        "grayish-very":"var(--very-dark-grayish-blue)",
+        "grayish-blue":"var(--dark-grayish-blue)",
+        "grayish-blue":"var(--grayish-blue)",
+        "white-color":"var(--white)",
       }
     },
   },
