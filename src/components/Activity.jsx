0327c17d4df0bc660a578aaf8cb69e6eea0193a6
gracebir <grace.devolop@gmail.com>
2023-09-11 21:43:02 +0200

    activity text component

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Activity = () => {
+const Activity = ({ title, desc, order, color }) => {
   return (
-    <div>
-      activity
+    <div className={`${order} flex items-center px-10 lg:px-0 h-[50vh] lg:h-auto`}>
+      <div className='max-w-sm mx-auto flex flex-col gap-6 items-center text-center lg:text-start lg:items-start'>
+        <h1 className="font-fraunces text-4xl">{title}</h1>
+        <p className="text-grayish-dark">{desc}</p>
+        <div className='flex flex-col'>
+          <a className='font-fraunces uppercase z-50' href="#">Learn more</a>
+          <span className={`border-4 ${color} rounded-xl -mt-2`}></span>
+        </div>
+      </div>
     </div>
   )
 }
