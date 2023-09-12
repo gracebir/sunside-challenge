18e51bb390a46ac55805d571074918a323dc6084
gracebir <grace.devolop@gmail.com>
2023-09-12 13:01:18 +0200

    product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import ProductInfo from './ProductInfo'
 
 const Product = () => {
   return (
-    <div>
-      <h1>Product</h1>
+    <div className='grid sm:grid-cols-1 lg:grid-cols-2'>
+      <ProductInfo
+        bg={"bg-graphic-mob lg:bg-graphic-desk"}
+        title="Graphic design"
+        desc={` Great design makes you memorable. 
+        We deliver artwork that underscores your 
+        brand message and captures potential 
+        clients’ attention.`}
+      />
+       <ProductInfo
+        bg={"bg-photography-mob lg:bg-photography-desk"}
+        title="Photography"
+        desc={`Increase your credibility by getting 
+        the most stunning, high-quality photos 
+        that improve your business image.`}
+      />
     </div>
   )
 }
