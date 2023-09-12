3d005ad8f6625f126075eb8f9fbad99bd66a6440
gracebir <grace.devolop@gmail.com>
2023-09-12 13:27:05 +0200

    testimonial data

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { emily, thomas, jennie } from '../assets/index'
+import Testimonial from './Testimonial'
+
+const data = [
+  {
+    id: 1,
+    name: "Emily R.",
+    avatar: emily,
+    role: "Marketing Director",
+    testimial: `We put our trust in Sunnyside and 
+    they delivered, making sure 
+    our needs were met and deadlines were 
+    always hit.`
+  },
+  {
+    id: 2,
+    name: "Thomas S.",
+    avatar: thomas,
+    role: "Chief Operating Officer",
+    testimial: `Sunnyside’s enthusiasm coupled with their 
+    keen interest in our brand’s success made it a 
+    satisfying and enjoyable experience.`
+  },
+  {
+    id: 3,
+    name: "Jennie F.",
+    avatar: jennie,
+    role: "Business Owner",
+    testimial: `Incredible end result!
+     Our sales increased over 400% when 
+     we worked with Sunnyside. Highly recommended!`
+  },
+]
 
 const Testimonials = () => {
   return (
     <div>
-      testimonials
+      <h1></h1>
+      <div>
+        {data.map((item)=>(
+          <Testimonial {...item} key={item.id}/>
+        ))}
+      </div>
     </div>
   )
 }
