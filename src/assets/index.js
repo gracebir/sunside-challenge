f60300c25ec5f39f3d1fc8368b3feae3ace6151a
gracebir <grace.devolop@gmail.com>
2023-09-12 15:03:39 +0200

    remove some assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,36 +1,15 @@
-import logo from './logo.svg'
-import instagram from './icon-instagram.svg'
-import facebook from './icon-facebook.svg'
-import hamburger from './icon-hamburger.svg'
-import pinterest from './icon-pinterest.svg'
-import twitter from './icon-twitter.svg'
-import arrow from './icon-arrow-down.svg'
-import emily from './image-emily.png'
-import jennie from './image-jennie.png'
-import thomas from './image-thomas.png'
+import logo from "./logo.svg"
+import instagram from "./icon-instagram.svg"
+import facebook from "./icon-facebook.svg"
+import hamburger from "./icon-hamburger.svg"
+import pinterest from "./icon-pinterest.svg"
+import twitter from "./icon-twitter.svg"
+import arrow from "./icon-arrow-down.svg"
+import emily from "./image-emily.jpg"
+import jennie from "./image-jennie.jpg"
+import thomas from "./image-thomas.jpg"
 
-// for desktop
-import cone from './desktop/image-gallery-cone.jpg'
-import milkbottle from './desktop/image-gallery-milkbottles.jpg'
-import orange from './desktop/image-gallery-orange.jpg'
-import sugarcubes from './desktop/image-gallery-sugarcubes.jpg'
-import design from './desktop/image-gallery-design.jpg'
-import heroImg from './desktop/image-header.jpg'
-import photography from './desktop/image-photography.jpg'
-import standout from './desktop/image-stand-out.jpg'
-import transform from './desktop/image-transform.jpg'
 
-// for mobile
-
-import coneMob from './mobile/image-gallery-cone.jpg'
-import milkbottleMob from './mobile/image-gallery-milkbottles.jpg'
-import orangeMob from './mobile/image-gallery-orange.jpg'
-import sugarcubesMob from './mobile/image-gallery-sugar-cubes.jpg'
-import designMob from './mobile/image-gallery-design.jpg'
-import heroImgMob from './mobile/image-header.jpg'
-import photographyMob from './mobile/image-photography.jpg'
-import standoutMob from './mobile/image-stand-out.jpg'
-import transformMob from './mobile/image-transform.jpg'
 
 export {
     logo,
@@ -42,23 +21,5 @@ export {
     arrow,
     emily,
     jennie,
-    thomas,
-    cone,
-    milkbottle,
-    orange,
-    sugarcubes,
-    design,
-    heroImg,
-    photography,
-    standout,
-    transform,
-    coneMob,
-    milkbottleMob,
-    orangeMob,
-    sugarcubesMob,
-    designMob,
-    heroImgMob,
-    photographyMob,
-    standoutMob,
-    transformMob
+    thomas
 }
